ffe3b8ffec9747b28926c565a2ff6d57909b19d9
Namekuji <nmkj@waah.day>
2023-09-18 01:17:42 -0400

    use jemalloc

--- packages/backend/native-utils/scylla-migration/src/main.rs
+++ packages/backend/native-utils/scylla-migration/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 use scylla_migration::{cli::run_cli, error::Error};
 
 #[tokio::main]
